ce2f3ed11d0de7fe9ba992f8a8f3b08e49aa685a
Denis <denis@moedelo.by>
2011-11-01 13:41:56 +0300

    fixed logout on search bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
 
-  	if params['search-keywords']
+  	if params['search-keywords'] != "" and params['search-keywords'] != nil
   		@search = Company.search do
 			fulltext params['search-keywords']
 		end
