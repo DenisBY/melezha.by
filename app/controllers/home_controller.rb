f7fea4ef9f62e073213ea8e5726e7cabc3dfbed0
Denis <denis@moedelo.by>
2011-11-11 19:11:38 +0300

    правки

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
 
-  	if params['search-keywords'] != "" and params['search-keywords'] != nil
+  	if params['q'] != "" and params['q'] != nil
   		# query = params['search-keywords'].split(/'([^']+)'|"([^"]+)"|\s+|\+/).reject{|x| x.empty?}.map{|x| x.inspect }*' && '
-  		query = params['search-keywords']
+  		query = params['q']
   		@companies = Company.search query, :star => true, :match_mode => :any
 		# @companies = @search.results
   	end
