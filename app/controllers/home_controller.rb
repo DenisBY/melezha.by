dc5985233516c7535e503c91df8cc4ec8e471bb6
Denis <denis@moedelo.by>
2011-12-02 09:37:49 +0300

    небольшой редизайн. логика поиска изменена на extended. расширено поле описания компании

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   	if params['q'] != "" and params['q'] != nil
   		# query = params['search-keywords'].split(/'([^']+)'|"([^"]+)"|\s+|\+/).reject{|x| x.empty?}.map{|x| x.inspect }*' && '
   		query = params['q']
-  		@companies = Company.search query, :star => true, :match_mode => :any
+  		@companies = Company.search query, :star => true, :match_mode => :extended
 		# @companies = @search.results
   	end
   	  	
